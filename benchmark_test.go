7c5b84850b366613c941658610dbd650ad871513
zergon321 <maximgradan@gmail.com>
2023-07-26 17:47:49 +0300

    Library updated.

---
+++ benchmark_test.go
@@ -0,0 +1,87 @@
+package mempool_test
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/zergon321/mempool"
+)
+
+type ByteArray []byte
+
+func (array ByteArray) Erase() error {
+	for i := 0; i < len(array); i++ {
+		array[i] = 0
+	}
+
+	return nil
+}
+
+func BenchmarkSyncPool(b *testing.B) {
+	pool := &sync.Pool{
+		New: func() any { return make([]byte, 64) },
+	}
+
+	for i := 0; i < b.N; i++ {
+		pool.Put(make([]byte, 64))
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		pool.Get()
+	}
+}
+
+func BenchmarkSyncPoolFill(b *testing.B) {
+	pool := &sync.Pool{
+		New: func() any { return make([]byte, 64) },
+	}
+
+	for i := 0; i < b.N; i++ {
+		pool.Put(make([]byte, 64))
+	}
+}
+
+func BenchmarkMempool(b *testing.B) {
+	pool, _ := mempool.NewPool[ByteArray](
+		func() ByteArray { return make([]byte, 64) })
+
+	for i := 0; i < b.N; i++ {
+		pool.Put(make([]byte, 64))
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		pool.Get()
+	}
+}
+
+func BenchmarkMempoolFill(b *testing.B) {
+	pool, _ := mempool.NewPool[ByteArray](
+		func() ByteArray { return make([]byte, 64) })
+
+	for i := 0; i < b.N; i++ {
+		pool.Put(make([]byte, 64))
+	}
+}
+
+func BenchmarkMempoolRefill(b *testing.B) {
+	pool, _ := mempool.NewPool[ByteArray](
+		func() ByteArray { return make([]byte, 64) })
+
+	for i := 0; i < b.N; i++ {
+		pool.Put(make([]byte, 64))
+	}
+
+	for i := 0; i < b.N; i++ {
+		pool.Get()
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		pool.Put(make([]byte, 64))
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/zergon321/mempool
 
 go 1.20
 
-require github.com/zergon321/ll v0.0.0-20230724222516-80dcef1cc080
+require github.com/zergon321/ll v0.0.0-20230724232506-2aa17996b403
